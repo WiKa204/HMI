1e2f2475f0cf23180d32df416bd7140c5af36c6b
WiKa204 <klaudiasagat@gmail.com>
2022-05-10 22:44:00 +0200

    W2 - half

--- W2.py
+++ W2.py
@@ -26,19 +26,52 @@ def zad2():
 # TODO Zaprojektowanie filtru FIR używając metody okien, który wytnie składowe powyżej 1200 Hz
 def zad3():
     fs = 48000
-    N = 100
+    N = 101 # 501 # zwiększenie liczby próbek wprowadza opóźnienie
     h = sig.firwin2(N, [0, 1200, 1400, fs/2], [1, 1, 0, 0], N+1, window=None, fs=fs)
     plt.plot(h)
-    plt.title('Odpowiedź impulsowa filtru')
+    plt.title('Odpowiedź impulsowa filtru - okno prostokątne') # NA KRAŃCACH WIDAĆ NIECIĄGŁOŚCI
+    plt.show()
+    h = sig.firwin2(N, [0, 1200, 1400, fs / 2], [1, 1, 0, 0], N + 1, window='blackman', fs=fs)
+    plt.plot(h)
+    plt.title('Odpowiedź impulsowa filtru - okno Blackmana')
+    plt.show()
+    h = sig.firwin2(N, [0, 1200, 1400, fs / 2], [1, 1, 0, 0], N + 1, window='hann', fs=fs)
+    plt.plot(h)
+    plt.title('Odpowiedź impulsowa filtru - okno Hanna')  # NA KRAŃCACH WYGŁADZENIE
+    plt.show()
+
+
+    # Zastosowanie filtru
+    y = sig.lfilter(h, 1, x)
+    plt.plot(y, label='po')
+    plt.plot(x, label='przed')
+    plt.title('Sygnał przed i po filtracji')
+    plt.legend()
+    plt.show()
+
+    # Analiza za pomocą transformaty Fouriera
+    plt.plot(xf, np.abs(fft(y)), label='po')
+    plt.plot(xf, np.abs(fft(x)), label='przed')
+    plt.title('Sygnał przed i po filtracji - transformata Fouriera')
+    plt.legend()
+    plt.show()
+
+    # metoda do analizy częstotliwościowej
+    w, hf = sig.freqz(h, worN=248, fs=fs)   # odp. imp. filtra, rozdzielczość (ilość próbek), fs sygnału
+    plt.plot(w, 20*np.log10(np.abs(hf)))      # przedstawienie w skali logarytmicznej
+    plt.title('Analiza częstotliwościowa')
+    plt.show()
+
+    # Polepszenie filtra, by mocniej tłumił - metoda najmniejszych kwadratów
+    h = sig.firls(N, [0, 1200, 1400, fs / 2], [1, 1, 0, 0], fs=fs)
+    plt.plot(h)
+    plt.title('Odpowiedź impulsowa filtru - metoda najmniejszych kwadratów')
     plt.show()
 
 def zad4():
     fs = 48000
     N = 1001
     h = sig.firwin2(N, [0, 1200, 1400, fs / 2], [1, 1, 0, 0], N + 1, window=None, fs=fs)
-    plt.plot(h)
-    plt.title('Odpowiedź impulsowa filtru z wydłużoną liczbą próbek')
-    plt.show()
 
     plt.plot(np.abs(fft(h)))
     plt.title('Charakterystyka filtru')
@@ -47,6 +80,7 @@ def zad4():
 def main():
     # zad2()
     zad3()
+    # zad4()
 
 if __name__ == '__main__':
     main()
