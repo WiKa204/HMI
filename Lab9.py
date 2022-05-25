7ea2fa1efdac4be2fc3827da21551d8da74036b9
WiKa204 <klaudiasagat@gmail.com>
2022-05-25 11:49:14 +0200

    Lab 9 - finished

--- Lab9.py
+++ Lab9.py
@@ -1,28 +1,28 @@
 import numpy as np
 import pandas as pd
-from scipy import signal
+from scipy import signal as sig
 from sklearn.cluster import KMeans
 from sklearn.datasets import make_blobs
 from sklearn.metrics import confusion_matrix
 
 
-def rms(signal: np.array, window=500, stride=100, fs=5120,
+def rms(signal, window=500, stride=100, fs=5120,
         columns_emg=['EMG_8', 'EMG_9']):  # wartści długości okna i przesunięcia w [ms]
     for column in columns_emg:
         # print(column)
-        data = signal[column]
+        data = signal[column].values
         # print(data)
-        x_0 = np.mean(data)
+        # x_0 = np.mean(data)
         x_diff =0
-        print(x_0)
+        # print(x_0)
         for i in range(0, len(data)-window, stride):
             # print(i)
             for n in range(i, i+window, 1):
-                print()
-                # x_diff += data[n:] - x_0
+                # print(data[n])
+                # x_diff += data[n] - x_0
+                x_diff += pow(data[n], 2)
                 # print(x_diff)
-            # xc = np.cumsum(abs(signal) ** 2)
-            # rms_i = np.sqrt((xc[window:] - xc[:-window]) / window)
+            rsm_i = np.sqrt(x_diff/window)
     return signal[columns_emg].iloc[int(window / 1000 * fs)::int(stride / 1000 * fs)]
 
 
@@ -32,22 +32,34 @@ def zc(signal, threshold: float = 0.1, window: float = 500, stride: float = 100,
         # s3 = np.sign(tsed)
         # s3[s3 == 0] = -1  # replace zeros with -1
         # zcs = len(np.where(np.diff(s3))[0])
+    for column in columns_emg:
+        # print(column)
+        data = signal[column].values
+        zc = 0
+        data_filtred = data[(data > threshold) | (data < - threshold)]
+        for i in range(0, len(data) - window, stride):
+            for n in range(i+1, i + window, 1):
+                sign = data[n]*data[n-1]
+                if(sign<0):
+                    zc += 1
+        print(zc)
     return signal[columns_emg].iloc[int(window / 1000 * fs)::int(stride / 1000 * fs)]
 
 
 def find_threshold(signal, columns_emg=['EMG_8', 'EMG_9'], column_gesture='TRAJ_GT', idle_gesture_id=0):
     thresholds = {}
-    for ch in columns_emg:
-        ts_ch = signal[signal[column_gesture] == idle_gesture_id][ch].abs().mean()
-        thresholds[ch] = ts_ch
-    thresholds_df = pd.DataFrame(thresholds)
+    for column in columns_emg:
+        ts_column = signal[signal[column_gesture] == idle_gesture_id][column].abs().mean()
+        thresholds[column] = ts_column
+    # thresholds_df = pd.DataFrame(thresholds)
+    # print(thresholds_df)
     return signal.loc[signal[column_gesture] == idle_gesture_id, columns_emg].mean()
 
 
 def norm_emg(signal, norm_coeffs, columns_emg=['EMG_8', 'EMG_9']):
     coeffs = norm_coeffs[columns_emg]
 
-    norm_sig = signal / norm_coeff
+    # norm_sig = signal / norm_coeff
     return signal[columns_emg]
 
 
@@ -63,8 +75,8 @@ data = pd.read_hdf('Lab10i11_Interface/mvc.hdf5')
 feature_rms = rms(train, window=500, stride=100, fs=5120,
                   columns_emg=['EMG_8', 'EMG_9'])  # wartści długości okna i przesunięcia w [ms]
 
-# feature_zc = zc(signal, threshold=0.1, window=500, stride=100, fs=5120,
-  #              columns_emg=['EMG_8', 'EMG_9'])  # wartści długości okna i przesunięcia w [ms]
+# feature_zc = zc(train, threshold=0.1, window=500, stride=100, fs=5120,
+ #             columns_emg=['EMG_8', 'EMG_9'])  # wartści długości okna i przesunięcia w [ms]
 
 # TODO 3: Wyznacz wartość progu dla funkcji zc - próg to 95% wszystkich próbek szumu
-# threshold = find_threshold(signal, columns_emg=['EMG_8', 'EMG_9'], column_gesture='TRAJ_GT', idle_gesture_id=0)
+threshold = find_threshold(train, columns_emg=['EMG_8', 'EMG_9'], column_gesture='TRAJ_GT', idle_gesture_id=0)
